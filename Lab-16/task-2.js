77a516a534481432cdcec6f8518284807e216c29
TheVSAKeeper <votintcev1@mail.ru>
2024-02-14 02:30:31 +0300

    Refactoring.

--- Lab-16/task-2.js
+++ Lab-16/task-2.js
@@ -1,5 +1,22 @@
-function addTask() {
-  let taskInput = document.getElementById("taskInput");
+const list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  (ev) => {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false,
+);
+
+function addItem(button) {
+  const list = button.parentElement.parentElement;
+  const itemsContainer = list.getElementsByClassName(
+    "w3-ul w3-border w3-margin-bottom",
+  )[0];
+
+  const taskInput = list.getElementsByClassName("w3-input w3-margin-bottom")[0];
+
   let task = taskInput.value.trim();
 
   if (task === "") {
@@ -7,40 +24,82 @@ function addTask() {
     return;
   }
 
-  let taskList = document.createElement("ul");
-  taskList.innerHTML = `
-            <li>
-                <input type="checkbox">
-                <span>${task}</span>
-                <button class="delete-task-btn" onclick="deleteTask(this)">Удалить</button>
-            </li>
-        `;
+  const newItem = document.createElement("li");
+  newItem.className = "w3-bar w3-border-white w3-border w3-hover-border-green";
+  newItem.innerHTML = `<span class="w3-bar-item"> ${task} </span>
+            <button
+              class="w3-bar-item w3-button w3-round w3-hover-red w3-right"
+              onclick="deleteItem(this)"
+            >
+              &times;
+            </button>
+           `;
 
-  document.getElementById("lists").appendChild(taskList);
+  itemsContainer.appendChild(newItem);
   taskInput.value = "";
 }
 
-let listId = 0;
-
 function addList() {
-  let taskInput = document.getElementById("task-list-input");
-  let task = taskInput.value.trim();
+  let titleInput = document.getElementById("list-title-input");
+  let title = titleInput.value.trim();
 
-  if (task === "") {
+  if (title === "") {
     alert("Введите название списка дела.");
     return;
   }
 
-  let list = document.createElement("option");
-  listId++;
-  list.value = listId;
-  list.id = `task-list-${listId}`;
-  list.textContent = `[${listId}] ${task}`;
+  const listsContainer = document.getElementById("lists-container");
 
-  document.getElementById("tasks-lists").appendChild(list);
+  const newList = document.createElement("div");
+  newList.className = "w3-card-4 w3-margin-bottom";
+  newList.innerHTML = `
+            <header class="w3-container w3-blue">
+              <h3>${title}</h3>
+            </header>
+
+            <div class="w3-container w3-margin-top">
+              <ul class="w3-ul w3-border w3-margin-bottom">
+              </ul>
+            </div>
+
+            <div class="w3-bar w3-padding">
+              <input
+                class="w3-input w3-margin-bottom"
+                placeholder="Введите дело"
+                type="text"
+              />
+
+              <button
+                class="w3-bar-item w3-button w3-round w3-border w3-border-green"
+                onclick="addItem(this)"
+              >
+                Добавить дело
+              </button>
+              <button
+                class="w3-bar-item w3-right w3-button w3-round w3-border w3-border-red"
+                onclick="deleteList(this)"
+              >
+                Удалить список
+              </button>
+            </div>
+    `;
+
+  listsContainer.addEventListener(
+    "click",
+    (event) => {
+      if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+      }
+    },
+    false,
+  );
+
+  listsContainer.appendChild(newList);
+
+  titleInput.value = "";
 }
 
-function deleteTask(button) {
+function deleteItem(button) {
   let taskItem = button.parentNode;
   taskItem.parentNode.removeChild(taskItem);
 }
--- Lab-16/task-3.js
+++ Lab-16/task-3.js
@@ -17,4 +17,26 @@ function calculate() {
   }
 
   document.getElementById("display").value = result;
+  return result;
+}
+
+function calculateAction(action) {
+  let result = calculate();
+  document.getElementById("display").value = action(result);
+}
+
+function decimalToBinary(decimal) {
+  return (decimal >>> 0).toString(2);
+}
+
+function binaryToDecimal(binary) {
+  return parseInt(binary, 2);
+}
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 }
